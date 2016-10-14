6ca28ad91101f97134624126567bac0ad69037b8
hsinewu <extraordinary1022@gmail.com>
2016-10-14 14:29:07 +0800

    finish Vertext shader

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -8,5 +8,5 @@ void main() {
 
   //TODO: rotate position by theta radians about the origin
 
-  gl_Position = vec4(position, 0, 1.0);
+  gl_Position = vec4(position.x*cos(theta)-position.y*sin(theta), position.x*sin(theta)+position.y*cos(theta), 0, 1.0);
 }
